6f9ffc19c4458a7db0220c3ae735f2fbb8d022f8
wev2530 <wev049665@gmail.com>
2024-09-21 22:36:43 +0100

    modified:   index.html

---
+++ index.js
@@ -0,0 +1,16 @@
+function linkhome() {
+    window.location.href ="index.html";
+}
+function linkabout() {
+    window.location.href ="index.html";
+}
+function linkreg() {
+    window.location.href ="index.html";
+}
+function linkgmithome() {
+    window.location.href ="https://greatmindsintechfoundation.webflow.io";
+}
+function linkspeakers() {
+    window.location.href ="#group";
+}
+
